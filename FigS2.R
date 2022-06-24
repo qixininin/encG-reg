1329bd3772e84b7f7eea2be59e206b6d4bdd5bbd
qixininin <60105717+qixininin@users.noreply.github.com>
2022-06-24 10:26:19 +0800

    Add files via upload

--- FigS2.R
+++ FigS2.R
@@ -30,8 +30,8 @@ for(r in 0:3)
     Gr = GenerateGeno_r(freq, n1, r, FALSE)
     X1 = Gr[[1]]
     X2 = Gr[[2]]
-    X1 = scale(X1)
-    X2 = scale(X2)
+    X1 = t(apply(X1, 1, scale))
+    X2 = t(apply(X2, 1, scale))  
     
     ## GRM calculated from individual genotype
     K12 = tcrossprod(X1, X2) / M
@@ -40,12 +40,12 @@ for(r in 0:3)
   dt = data.frame(grp = rep(c("Theoretical", "Observed"), each = Mnum),
                   r = r,
                   M = as.factor(rep(Mvec, 2)),
-                  var = c((1+(0.25)^r)/Mvec, rst))
+                  var = c((1-(0.25)^r)/Mvec, rst))
   p[[r+1]] = ggplot(data = dt, aes(x=M, y=var, color=grp, fill=grp))+
     geom_bar(stat="identity", width = 0.6, position=position_dodge(width = 0.8))+
     scale_color_manual(values=color, name="") +
     scale_fill_manual(values=color, name="") +
-    scale_y_continuous(limits = c(0,0.003))+
+    scale_y_continuous(limits = c(0,0.0015))+
     labs(title = title,
          y="Var(GRM)")+
     theme_article()+
