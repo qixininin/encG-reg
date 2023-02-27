e5a02eda80dc13b37a46e6d9b5615039ff78b852
qixininin <60105717+qixininin@users.noreply.github.com>
2023-02-27 13:29:26 +0800

    Update and rename Fig2.R to Fig3.R

--- 1-Simulations/Fig3.R
+++ 1-Simulations/Fig3.R
@@ -1,4 +1,4 @@
-## Figure 2 Validation for the sampling variance for GRM, encGRM, and encG-reg
+## Figure 3 Validation for the sampling variance for GRM, encGRM, and encG-reg
 ## encGRMsource.R includes functions:
 ##                RandomMatrixEncryption()
 ##                GenerateGeno()
